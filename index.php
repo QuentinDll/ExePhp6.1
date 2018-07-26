283f69d18e83cf7c6023825d9f0548c07ddec6e8
QuentinDll <quentin.delplace13@gmail.com>
2018-07-26 16:22:11 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Exercice 6</title>
+</head>
+<body>
+<p>
+<?php
+$name = 'Hill';
+echo "Bonjour ";
+echo $name;
+echo ", comment vas tu ?"
+ ?>
+</p>
+</body>
+</html>
